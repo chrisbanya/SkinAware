5224c776da53498545612462b5b0eef3b118be62
Adabanya Christian <adabanyachristian21@gmail.com>
2025-07-23 03:55:24 +0100

    initial commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import FileUpload from "../components/FileUpload";
+import SkinCategories from "../components/SkinCategories";
+import NavBar from "../UI/NavBar";
+
+const HomePage = () => {
+  return (
+    <div className="w-full">
+      <NavBar />
+      <hr className="w-full outline-gray-950 mb-5" />
+      <div className="w-11/12 lg:w-10/12 max-w-screen-lg mx-auto ">
+        <h2 className="my-6 text-lg font-bold">Welcome Guest</h2>
+        <FileUpload />
+        <hr className="w-1/2 mx-auto mt-10 mb-5 outline-gray-950" />
+        <SkinCategories />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
