246f90309d69658c0b3074c7d14e56d3f6f33b22
Adabanya Christian <adabanyachristian21@gmail.com>
2025-07-26 03:30:41 +0100

    added: reusable component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
+
 import FileUpload from "../components/FileUpload";
 import SkinCategories from "../components/SkinCategories";
+import HowItWorksSection from "../UI/HowItWorksSection";
 import NavBar from "../UI/NavBar";
 
 const HomePage = () => {
@@ -11,7 +13,9 @@ const HomePage = () => {
         <h2 className="my-6 text-lg font-bold">Welcome Guest</h2>
         <FileUpload />
         <hr className="w-1/2 mx-auto mt-10 mb-5 outline-gray-950" />
+        <HowItWorksSection overRideClass={"w-full"}/>
         <SkinCategories />
+       
       </div>
     </div>
   );
